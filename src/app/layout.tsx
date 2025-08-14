ae826ec03cc1b718a679344a94ff37edf121306a
H G <hectorangel2001@gmail.com>
2025-08-14 19:13:11 -0400

    fix: add seo images

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,6 +4,7 @@ import "./globals.css";
 
 export const metadata: Metadata = {
   title: "Crystal Chapel",
+  manifest: "/seo/site.webmanifest",
   description:
     "Crystal Chapel specializes in creating exquisite religious-themed illustrations that combine artistry, spirituality, and elegance. Our unique designs bring sacred stories and symbols to life, crafted with attention to detail and deep cultural meaning.",
 };
@@ -15,6 +16,26 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/seo/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="/seo/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="/seo/favicon-16x16.png"
+        />
+      </head>
+
       <body>{children}</body>
     </html>
   );
